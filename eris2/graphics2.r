e4140a37eedc86db87f7d4bfe8224cacde6e718a
Thomas Browne <thomas.browne@exoduspoint.com>
2019-10-17 19:08:05 +0100

    with pnli

--- eris2/graphics2.r
+++ eris2/graphics2.r
@@ -42,10 +42,46 @@ cashflows2_chart <- function(topng = F) {
     if(topng) dev.off()
 }
 
+zero_charts<- function(topng = F) {
+    chartsize <- c(9, 5)
+    if(topng) {
+        png("zeros.png", width = chartsize[1], height = chartsize[2], 5, units = "in", res = 200, pointsize = 12)
+    } else {
+        windows(chartsize[1], chartsize[2])
+    }
+    data <- read.csv("cashflows2.csv")
+    discnov <- as.numeric(read.csv("discnov3018.txt", header = F)[, 1])
+    discoct <- as.numeric(read.csv("discoct1019.txt", header = F)[, 1])
+    zeronov <- as.numeric(substr(read.csv("zeronov3018.txt", header = F)[, 1], 1, 8))
+    zerooct <- as.numeric(substr(read.csv("zerooct1019.txt", header = F)[, 1], 1, 8))
+    data["discnov"] <- discnov
+    data["discoct"] <- discoct
+    data["zeronov"] <- zeronov
+    data["zerooct"] <- zerooct
+    data[, "Payment.Date"] <- as.Date(data[, "Payment.Date"])
+    fixed <- data[data[, "Is.Fixed"] == "True", ]
+    floating <- data[data[, "Is.Fixed"] == "False", ]
+    plot(floating[, 2], floating[, "zeronov"], col = "green3", type = "b", xlab = "", ylab = "", ylim = c(1.4, 3), lwd = 2)
+    points(floating[, 2], floating[, "zerooct"], col = "darkgoldenrod2", type = "b", xlab = "", ylab = "", lwd = 2)
+    legend("bottomleft", fill = c("green3", "darkgoldenrod2"), legend = c("November 2018", "October 2019"))
+    if(topng) dev.off()
+    if(topng) {
+        png("discounts.png", width = chartsize[1], height = chartsize[2], 5, units = "in", res = 200, pointsize = 12)
+    } else {
+        windows(chartsize[1], chartsize[2])
+    }
+    barplot(as.matrix(floating[, c("discnov", "discoct")]), beside = T, ylim = c(0, 1), 
+         names.arg = unlist(rep(floating[, "Payment.Date"], 2)), las = 2, cex.names = 0.9, 
+         col = c(rep("green3", 20), rep("darkgoldenrod2", 20)))
+    abline(h = 1, lty = "dashed")
+    if(topng) dev.off()
+
+}
 
 rr <- function(topng = F) {
     cashflows_chart(topng = topng)
     cashflows2_chart(topng = topng)
+    zero_charts(topng = topng)
 }
     
 
